266f9c491d666d4b8d56b2e39453c218a1ce8c7a
ilyatroshko <troshko.00@mail.ru>
2021-02-17 15:27:46 +0300

    input

---
+++ src/main/java/diplom/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,32 @@
+package diplom.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/registration",
+                        "/registrationClientInfo",
+                        "/about",
+                        "/static/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
---
+++ target/classes/templates/parts/navbar.ftl
@@ -0,0 +1,10 @@
+<#include "security.ftl">
+<#import "login.ftl" as l>
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ccfff7;">
+    <a class="navbar-brand" href="/">Управление остатками</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</nav>
