25cbf128127db06bf77b16c40368a8db89879238
ilyatroshko <troshko.00@mail.ru>
2021-02-18 13:13:38 +0300

    input

--- src/main/java/diplom/TroshkoApplication.java
+++ src/main/java/diplom/TroshkoApplication.java
@@ -2,9 +2,12 @@ package diplom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
+@EntityScan("diplom.model")
+@EnableJpaRepositories("diplom.reposit")
 public class TroshkoApplication {
 
     public static void main(String[] args) {
--- src/main/java/diplom/reposit/UserReposit.java
+++ src/main/java/diplom/reposit/UserReposit.java
@@ -1,11 +1,12 @@
 package diplom.reposit;
 
-
 import diplom.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserReposit extends CrudRepository<User, Long> {
     User findByUsername(String username);
     List<User> findByActive(boolean active);
